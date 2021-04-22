0c11f364076253b9b0293f7d29110f1267f2d932
Mihran <xachatryan200104@gmail.com>
2021-04-23 01:03:04 +0400

    8

--- src/pages/home/footer/Source.js
+++ src/pages/home/footer/Source.js
@@ -2,10 +2,20 @@ import "./styles.css";
 
 export default function Source() {
   return (
-    <div>
+    <div className="carousel-content">
       <h3> Recent Posts </h3>
+      <i className="icon-twitter footer-icon" />
       <h4> 3 Common Resume Mistakes Most People Make </h4>
-      <p> Using the Same Resume For Multiple Job Applications </p>
+      <h4> 1.Typos and Grammatical Errors </h4>
+      <p>
+        {" "}
+        Yes, we know, it’s probably the most obvious of all <br />
+        resume tips: It needs to be grammatically perfect. If your
+        <br /> resume isn't, employers will read between
+        <br /> the lines and draw not-so-flattering conclusions
+        <br /> about you, like, "This person can't write," or, "This person
+        obviously doesn't care."{" "}
+      </p>
     </div>
   );
 }
